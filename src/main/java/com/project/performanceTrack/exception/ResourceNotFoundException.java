9e7b3a4d34d1a0c6dca503f46ba28f9e4529ad95
Ankit bhardwaj <101131763+monster0freason@users.noreply.github.com>
2026-02-18 19:43:41 +0530

    removed monolith code

--- src/main/java/com/project/performanceTrack/exception/ResourceNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.project.performanceTrack.exception;
-
-// Custom exception for resource not found
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
